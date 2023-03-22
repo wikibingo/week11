79ff5fb288aaa5f62e6c37df0ddd28ba8ad60f68
Wenli <wikibingo@aim.com>
2023-03-22 12:54:29 -0700

    circle calculation function

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,33 @@ greet(input)
 const greet = (name, time = "day") =>{
     console.log(`Hello ${time} ${name}!`)
 };
+
+const calArea = (r) => {
+    const pi = 3.14159;
+    if(isNaN(r) === false){
+        const area = pi * r **2;
+        alert(`The area of a circle with radius ${r} is ${area}`);
+        return area;
+    }
+    else{console.log('Invalid input')}
+}
+
+let r = prompt('enter a radius:');
+calArea(r);
+//----------------------------------------------------------------------------
+//neda example
+function calCircleArea(radius)
+{
+    if(isNaN(radius)){
+        alert("This is not a number");
+        //return undefined by default
+    }else{
+        const area = Math.PI * radius **2;
+        return area;
+    }
+}
+
+let userValue = prompt("enter a number:");
+//prompt always returns a string
+let result = calCircleArea(userValue);
+alert(`The area of a circle with radius ${userValue} is ${result.toFixed(2)}`)
