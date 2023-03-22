c556f4ab5e0a75d711fe8f4d6bbab8f5fffcb13c
Wenli <wikibingo@aim.com>
2023-03-22 12:30:03 -0700

    three functions declearation

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 function greet (name){
     console.log(`Hello ${name}!`)
-}
+};
 
+const greet = function(name){
+    console.log(`Hello ${name}!`)
+};
+
+const greet = (name) =>{
+    console.log(`Hello ${name}!`)
+};
+
+const greet = name => console.log(`Hello ${name}!`); #can remove parenthesis if only one parameter, can remove brackets if only one statement 
+
+let input = prompt("enter name:");
+greet(input)
